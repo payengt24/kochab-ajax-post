d9caeefee030afaa4cc6cfc69d7c16c29a566f63
Pa Yeng Thao <payengt24@gmail.com>
2018-04-19 14:53:26 -0500

    lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,20 +4,46 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery has been loaded');
+    $('#addNewSong').on('click', addNew)
+    getAllSongs();
+    addNew();
+}
+
+function getAllSongs() {
     $.ajax({
         method: 'GET',
         url: '/records'
     })
-    .then(function (response){
-        console.log('my response', response)
-        response.forEach(function (record) {
-            $('#recordList').append(`<tr>
+        .then(function (response) {
+            console.log('my response', response)
+            $('#recordList').empty();
+            response.forEach(function (record) {
+                $('#recordList').append(`<tr>
             <td>${record.title}</td>
             <td>${record.artist}</td>
             <td>${record.year}</td>
             <td>${record.cost}</td>
             </tr>`);
-        });
-    })
-
+            });
+        })
 }
+function addNew() {
+    const newSong = {
+        title: $('#newSongTitle').val(),
+        artist: $('#newSongArtist').val(),
+        year: $('#newSongYear').val(),
+        cost: $('#newSongCost').val()
+    };
+    console.log('New song object', newSong);
+    $.ajax({
+        method: 'POST',
+        url: '/add-record',
+        data: newSong //the object
+    })
+        .then(function (response) {
+            console.log('my repose: ', response);
+            getAllSongs();
+        });
+}
+
+
