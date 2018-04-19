82c697598893435710ef87475e9ecd8ee9a5b2c3
Pa Yeng Thao <payengt24@gmail.com>
2018-04-19 12:01:14 -0500

    add list on dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,20 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery has been loaded');
+    $.ajax({
+        method: 'GET',
+        url: '/records'
+    })
+    .then(function (response){
+        console.log('my response', response)
+        response.forEach(function (record) {
+            $('#recordList').append(`<tr>
+            <td>${record.title}</td>
+            <td>${record.artist}</td>
+            <td>${record.year}</td>
+            <td>${record.cost}</td>
+            </tr>`);
+        });
+    })
+
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const PORT= 5000; //don't want to hard code the PORT that's why we created a variable
 
+//the below const will be in the data base but for now its here becuz we have not learn DB yet; when its long coding than we use a modules
+const recordCollection = require('./modules/record-collection'); //modules- source where the file is always put ./ first
+
 app.use(express.static('server/public')); //this shows everything in the public folder in the localhost5000
+
+app.get('/records', (req, res) => {
+    res.send(recordCollection)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
